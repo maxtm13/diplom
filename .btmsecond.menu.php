181eef94b4cc361ab8cad3bcc48feabbfd60c473
maxtm13 <maxtm1@yandex.ru>
2024-11-04 23:38:39 +0300

    add header footer menu

---
+++ .btmsecond.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "каталог",
+        "/catalog/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "о магазинк",
+        "/about/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "отзывы",
+        "/review/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "доставка и оплата",
+        "/delivery/",
+        Array(),
+        Array(),
+        ""
+    )
+);
+?>
---
+++ local/templates/main-page/description.php
@@ -0,0 +1,8 @@
+<?php
+
+$arTemplate = array(
+    'NAME' => 'main_page',
+    'DESCRIPTION' => 'Новый шаблон для главной страницы',
+    'SORT' => '',
+    'TYPE' => '',
+);
